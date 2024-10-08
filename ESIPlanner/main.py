978e9786ff5fd4a95035fca4e38f85b03e652397
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2024-10-08 19:54:22 +0200

    Update main.py

--- ESIPlanner/main.py
+++ ESIPlanner/main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.add(ft.SafeArea(ft.Text("Hello soy Carlos, Welcome!")))
 
 
 ft.app(main)
